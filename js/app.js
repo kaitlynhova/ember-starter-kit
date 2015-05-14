5220b7a71ca425235da6c28d2845017cd23b53cf
Kaitlyn Hova <kaitlynhova@gmail.com>
2015-05-14 14:43:28 -0700

    populating blog posts

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ App.Router.map(function(){
   this.resource('about');
   this.resource('posts');
 });
+
+App.PostsRoute = Ember.Route.extend({
+  model: function() {
+    return posts;
+  }
+});
+
+var posts = [{
+  id: '1',
+  title: "Rails is wat",
+  author: {name: "kaitlyn"},
+  date: new Date('12-27-2012'),
+  excerpt: "this is post 1",
+  body: "yooooooooo post one body"
+}, {
+  id: '2',
+  title: "Rails number 2",
+  author: {name: "kaitlyn"},
+  date: new Date('12-29-2012'),
+  excerpt: "this is post 2",
+  body: "yooooodfvdfgdgdfgdfg2222 2 22 2 2 2 oooo post one body"
+}];
